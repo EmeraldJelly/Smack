4747b5ebdae061b93b3fc19a57107951f3cb22eb
EmeraldJelly <nickb122717@icloud.com>
2018-01-13 17:15:19 -0500

    Added Stuffs
    + LoginVC and UI for it.

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Nicholas Brewster on 1/13/18.
+//  Copyright © 2018 Nicholas Brewster. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Nicholas Brewster on 1/13/18.
+//  Copyright © 2018 Nicholas Brewster. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
